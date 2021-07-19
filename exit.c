81f2562b57da5038bf4efd4eacb1f957774d32ef
Krazy Seed <kseed@mi-c5.msk.21-school.ru>
2021-07-19 18:44:04 +0300

    norminette improvement

---
+++ exit.c
@@ -0,0 +1,15 @@
+#include "philosophers.h"
+
+int 	clean_n_exit(t_main *main, int flag)
+{
+	if (main->philo_list)
+		ft_dlist_del(&main->philo_list, main->num_philo);
+	if (flag > 0)
+	{
+		pthread_mutex_destroy(&main->death);
+		pthread_mutex_destroy(&main->print);
+	}
+	else if (flag == 1 || flag == 0)
+		printf("%sError!%s\n", RED, RESET);
+	return (1);
+}
